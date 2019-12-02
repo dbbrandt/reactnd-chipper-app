24b35caee5c60a6c62d438402ae01fb12a7c2d2f
Daniel Brandt <dbbrandt@gmail.com>
2019-12-02 18:11:27 -0500

    Added simple routing.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleGetInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
 import LoadingBar from "react-redux-loading";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +16,16 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true
+          ? null
+          :
+          <Router>
+            <div>
+              <Route exact path='/' component={Dashboard}/>
+              <Route path='/new' component={NewTweet}/>
+            </div>
+          </Router>
+        }
       </div>
     );
   }
