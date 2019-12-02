ad22d42a9e30015a3327b4c19c14198aa1f6c2c9
Daniel Brandt <dbbrandt@gmail.com>
2019-12-02 18:12:07 -0500

    Factor out tweetlist for reuse. Add navbar.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import Tweet from "./Tweet";
+import { connect } from "react-redux";
+import NavBar from "./NavBar";
+import TweetList from "./TweetList";
+// import { Route, Router } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { tweetIds } = this.props
+    const { tweetIds } = this.props;
     return (
       <div className='container'>
-        <h3 className='center'>Your Timeline</h3>
-        <ul className='dashboard-list'>
-          {tweetIds.map((id) =>
-            <li key={id}>
-              <Tweet id={id}/>
-            </li>
-          )}
-        </ul>
+        <NavBar/>
+        <h3 className="center">Your Timeline</h3>
+        <TweetList tweetIds={tweetIds}/>
       </div>
-    )}
+    );
+  }
 }
 
 const mapStateToProps = ({ tweets }) => ({
-  tweetIds: Object.keys(tweets)
-    .sort((a,b) => tweets[a].timeStamp - tweets[b].timeStamp)
+  tweetIds: Object.keys(tweets).sort(
+    (a, b) => tweets[a].timeStamp - tweets[b].timeStamp
+  )
 });
 
 export default connect(mapStateToProps)(Dashboard);
