22813850e69744b236753ddb3a8eea79dee7d0e6
Daniel Brandt <dbbrandt@gmail.com>
2019-12-02 18:12:25 -0500

    First pass at nav bar without links.

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <ul>
+          <li>Home</li>
+          <li>New Tweet</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
+
+
